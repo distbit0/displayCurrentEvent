8f02583ba553c6f2900b2f11babf6431bc07745e
Dist Bit <adp01@pm.me>
2023-10-11 10:34:24 +1100

    main.py run.sh

--- main.py
+++ main.py
@@ -13,15 +13,6 @@ import webbrowser
 import time
 
 
-def open_in_brave(url):
-    # Run the process and capture the output
-    subprocess.run(
-        [getConfig()["browserPath"], url],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-
 def open_bookmarks(path_to_folder):
     # Load the bookmarks file
     with open(getConfig()["bookmarksFilePath"], "r") as f:
@@ -66,12 +57,13 @@ def getAbsPath(relPath):
 
 
 def openBookmarksForNewEvents(title):
-    if open("currentEvent.txt").read() == title:
+    pathToCurrentEventFile = getAbsPath("currentEvent.txt")
+    if open(pathToCurrentEventFile).read() == title:
         return
     quitBraveBrowser()
-    time.sleep(0.5)
+    # time.sleep(0.5)
     open_bookmarks("/Bookmarks bar/Open tabs/x" + title)
-    open("currentEvent.txt", "w").write(title)
+    open(pathToCurrentEventFile, "w").write(title)
 
 
 def main():
@@ -83,7 +75,6 @@ def main():
         not os.path.exists(getAbsPath(CACHE_FILE))
         or random.randint(1, 100) < getConfig()["cacheRefreshProbability"] * 100
     )
-
     if should_download:
         ical_string = urllib.request.urlopen(URL).read()
         with open(getAbsPath(CACHE_FILE), "wb") as f:
@@ -91,7 +82,6 @@ def main():
     else:
         with open(getAbsPath(CACHE_FILE), "rb") as f:
             ical_string = f.read()
-
     # Load calendar
     calendar = icalendar.Calendar.from_ical(ical_string)
 
@@ -99,7 +89,6 @@ def main():
     local_tz = pytz.timezone(getConfig()["timezone"])
     now = datetime.datetime.now(local_tz)
     events = recurring_ical_events.of(calendar).at(now)
-
     messageText = []
     # Display the event title and time remaining in a popup
     for event in events:
@@ -107,7 +96,6 @@ def main():
         openBookmarksForNewEvents(title)
         end_time = event["DTEND"].dt
         duration_seconds = int((end_time - now).total_seconds())
-
         hours = duration_seconds / 3600
         message = title + " " * 15 + str(round(hours, 1))
         messageText.append(message)
