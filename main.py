8707c9ee7c920530c3fb1bcad355d8f30b4ac6e5
Dist Bit <adp01@pm.me>
2023-10-11 10:11:58 +1100

    currentEvent.txt main.py

--- main.py
+++ main.py
@@ -8,7 +8,46 @@ import subprocess
 from os import path
 import json
 import pytz
-import sys
+import json
+import webbrowser
+import time
+
+
+def open_in_brave(url):
+    # Run the process and capture the output
+    subprocess.run(
+        [getConfig()["browserPath"], url],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+
+def open_bookmarks(path_to_folder):
+    # Load the bookmarks file
+    with open(getConfig()["bookmarksFilePath"], "r") as f:
+        bookmarks = json.load(f)
+
+    # Traverse the bookmarks tree
+    def traverse(node, path):
+        if "name" in node:
+            new_path = path + "/" + node["name"]
+            if new_path.lower() == path_to_folder.lower():
+                # We've found the folder, open all bookmarks in it
+                for child in node["children"]:
+                    if "url" in child:
+                        webbrowser.open(child["url"])
+            else:
+                # Keep looking
+                if "children" in node:
+                    for child in node["children"]:
+                        traverse(child, new_path)
+
+    # Start traversal from the root
+    traverse(bookmarks["roots"]["bookmark_bar"], "")
+
+
+def quitBraveBrowser():
+    subprocess.run(["killall", getConfig()["browerProcessName"]])
 
 
 def getConfig():
@@ -24,6 +63,15 @@ def getAbsPath(relPath):
     fullPath = path.abspath(path.join(basepath, relPath))
 
     return fullPath
+
+
+def openBookmarksForNewEvents(title):
+    if open("currentEvent.txt").read() == title:
+        return
+    quitBraveBrowser()
+    time.sleep(0.5)
+    open_bookmarks("/Bookmarks bar/Open tabs/x" + title)
+    open("currentEvent.txt", "w").write(title)
 
 
 def main():
@@ -56,11 +104,12 @@ def main():
     # Display the event title and time remaining in a popup
     for event in events:
         title = event.get("SUMMARY", "Unknown Event").upper()
+        openBookmarksForNewEvents(title)
         end_time = event["DTEND"].dt
         duration_seconds = int((end_time - now).total_seconds())
 
         hours = duration_seconds / 3600
-        message = title + " " * 10 + str(round(hours, 1)) + " hrs"
+        message = title + " " * 15 + str(round(hours, 1))
         messageText.append(message)
 
     messageText = "  ||  ".join(messageText)
